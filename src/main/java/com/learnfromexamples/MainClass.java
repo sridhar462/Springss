85f89c1323705868117a474ba5a78dc882ed0887
sridhar462 <sridhar462@gmail.com>
2020-02-03 20:52:38 +0530

    added tomcat to provided

--- src/main/java/com/learnfromexamples/MainClass.java
+++ src/main/java/com/learnfromexamples/MainClass.java
@@ -2,9 +2,11 @@ package com.learnfromexamples;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainClass<T,V> {
+public class MainClass extends SpringBootServletInitializer{
 /*	T first;
 	V second;
  
@@ -21,6 +23,12 @@ public class MainClass<T,V> {
 		System.out.println(object.getClass().get);
 	}*/
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(MainClass.class);
+	}
+	
 	public static void main(String[] args) {
 		 SpringApplication.run(MainClass.class, args);
 	}
